a9d70e397e06fefef02b276624c4784d15b1adb8
Luis María Hernández <luismariahz@gmail.com>
2022-11-06 16:59:02 -0300

    Rama de cambios en el código js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //ELEMENTOS HTML
 
 let btnBuscar = document.getElementById("buscar");
@@ -5,6 +7,7 @@ let contenedor = document.getElementById("reservaVisor");
 let btnIngresar = document.getElementById("ingresar");
 let areaUsuario = document.getElementById("areaUsuario");
 let misReservas = document.getElementById("misReservas");
+let btnCerrarSesion = document.getElementById("cerrarSesion");
 
 //CLASES
 
@@ -13,40 +16,17 @@ class Reservas {
     this.id = parseInt(id);
     this.src = src;
     this.nombre = nombre;
-    this.ingreso = new Date(ingreso);
-    this.egreso = new Date(egreso);
+    this.ingreso = ingreso;
+    this.egreso = egreso;
     this.adultos = parseInt(adultos);
     this.ninos = parseInt(ninos);
   }
 
   calcularDias() {
-    let cantDias = this.egreso.getDate() - this.ingreso.getDate();
+    let cantDias = this.egreso.diff(this.ingreso);
 
-    return cantDias;
-  }
+    return cantDias.as('days');
 
-  escribirIngreso() {
-    let formatoFinal =
-      this.ingreso.getDate() +
-      1 +
-      "/" +
-      (this.ingreso.getMonth() + 1) +
-      "/" +
-      this.ingreso.getFullYear();
-
-    return formatoFinal;
-  }
-
-  escribirEgreso() {
-    let formatoFinal =
-      this.egreso.getDate() +
-      1 +
-      "/" +
-      (this.egreso.getMonth() + 1) +
-      "/" +
-      this.egreso.getFullYear();
-
-    return formatoFinal;
   }
 
   calcularPrecio() {
@@ -66,8 +46,8 @@ const cabanas = [
     id: 1,
     src: "recursos/cabaña1.jpg",
     nombre: "Los Osos",
-    ingreso: "2022-10-3",
-    egreso: "2022-10-9",
+    ingreso: "2022-11-07",
+    egreso: "2022-11-13",
     adultos: 2,
     ninos: 2,
   },
@@ -75,8 +55,8 @@ const cabanas = [
     id: 2,
     src: "recursos/cabaña2.jpg",
     nombre: "Los Pumas",
-    ingreso: "2022-10-10",
-    egreso: "2022-10-16",
+    ingreso: "2022-11-14",
+    egreso: "2022-11-20",
     adultos: 7,
     ninos: 0,
   },
@@ -84,8 +64,8 @@ const cabanas = [
     id: 3,
     src: "recursos/cabaña3.jpg",
     nombre: "Los Álamos",
-    ingreso: "2022-10-17",
-    egreso: "2022-10-23",
+    ingreso: "2022-11-21",
+    egreso: "2022-11-27",
     adultos: 4,
     ninos: 4,
   },
@@ -93,82 +73,75 @@ const cabanas = [
     id: 4,
     src: "recursos/cabaña4.jpg",
     nombre: "Los vados",
-    ingreso: "2022-10-3",
-    egreso: "2022-10-9",
+    ingreso: "2022-11-07",
+    egreso: "2022-11-13",
     adultos: 2,
     ninos: 2,
   },
 ];
 const carrito = [];
-
 const reservas = [];
 
 //FUNCIONES
 
-function comprobarUsuario(){
+function comprobarUsuario() {
+  let mail = sessionStorage.getItem("mail");
+  let control = true;
 
-    let mail = sessionStorage.getItem('mail');
-    let control = true;
+  if (mail == null) {
+    Swal.fire({
+      title: 'Atención',
+      text: 'Para poder reservar tiene que ingresar al sistema',
+      icon: 'info'
+    }).then((resultado) =>{
+      if(resultado.isConfirmed){
+        location.href = "paginas/login.html";
+      }
+    }); 
+    control = false;   
+  }
 
-    if(mail == null){
-        alert('Para poder reservar tiene que ingresar al sistema');
-        control = false;
-
-        location.href ="paginas/login.html";
-    }
-
-    return control;
+  return control;
 }
 
 function guardarReserva(idReserva) {
-
   const item = reservas.find((res) => (res.id = idReserva));
   carrito.push(item);
 
   if (carrito.length != 0) {
-
     sessionStorage.setItem("carrito", JSON.stringify(carrito));
-    console.log(carrito);
+
     alert('Agregado a "Mis Reservas"');
 
-    location.href="paginas/mis-reservas.html";
+    location.href = "paginas/mis-reservas.html";
   }
-
 }
 
-function usuarioLoggeado(){
+function usuarioLoggeado() {
+  let mail = sessionStorage.getItem("mail");
 
-    let mail = sessionStorage.getItem('mail');
-
-    if(areaUsuario != null){
-
-        if(mail != null){
-            areaUsuario.innerHTML = `<nav class="header__nav-areausuario-nav">
+  if (areaUsuario != null) {
+    if (mail != null) {
+      areaUsuario.innerHTML = `<nav class="header__nav-areausuario-nav">
             <ul class="header__nav-areausuario-nav-lista">
                 <li class="header__nav-areausuario-nav-lista-item"><a href="#">${mail}</a></li>
-                <li class="header__nav-areausuario-nav-lista-item"><a href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a></li>
+                <li class="header__nav-areausuario-nav-lista-item"><button type="button" id="cerrarSesion"><i class="fa-solid fa-arrow-right-from-bracket"></i></button></li>
             </ul>
         </nav>`;
-
-        }else {
-            areaUsuario.innerHTML = `<a class="header__nav-areausuario-ingresar" href="paginas/login.html">Ingresar</a>`;
-        }
+    } else {
+      areaUsuario.innerHTML = `<a class="header__nav-areausuario-ingresar" href="paginas/login.html">Ingresar</a>`;
     }
+  }
 }
 
-function verMisReservas(){
+function verMisReservas() {
+  let mail = sessionStorage.getItem("mail");
 
-  let mail = sessionStorage.getItem('mail');
+  if (misReservas != null) {
+    if (mail != null) {
+      const carritoReservas = JSON.parse(sessionStorage.getItem("carrito"));
 
-  if(misReservas != null){
-
-    if(mail != null){
-      const carritoReservas = JSON.parse(sessionStorage.getItem('carrito'));
-
-      console.log(carritoReservas);
-
-      carritoReservas.forEach(cr => {
-
+      carritoReservas.forEach((cr) => {
         misReservas.innerHTML += `<article class="main__misreservas-visor-tarjeta">
         <img class="main__misreservas-visor-tarjeta-img" src="../${cr.src}">
         <table class="main__misreservas-visor-tarjeta-tabla">
@@ -176,85 +149,96 @@ function verMisReservas(){
                 <th>Nombre</th><th>Entrada</th><th>Salida</th><th>Adultos</th><th>Niños</th><th>Precio</th><th>Acciones</th>
             </thead>
             <tbody>
-                <td>${
-                  cr.nombre
-                }</td><td>${cr.ingreso}</td><td>${cr.egreso}</td><td>${
-          cr.adultos
-        }</td><td>${
-          cr.ninos
-        }</td><td>$-</td></td><td><button class="main__misreservas-visor-tarjeta-tabla-botonE agregar-carrito" id="boton${
-          cr.id
-        }">Eliminar</button></td>
+                <td>${cr.nombre}</td><td>${cr.ingreso}</td><td>${cr.egreso}</td><td>${cr.adultos}</td><td>${cr.ninos}</td><td>$-</td></td><td><button class="main__misreservas-visor-tarjeta-tabla-botonE" id="boton${cr.id}">Eliminar</button></td>
                         </tbody>
                     </table>
                 </article>`;
-
       });
-    }
-    else {
+    } else {
       misReservas.innerHTML += `<p class="main__reserva-visor-noResult">No hay reservas</p>`;
     }
   }
+}
+
+function configurarFechas() {
+
+  const DateTime = luxon.DateTime;
+
+  let fechas = document.querySelectorAll('input[type="date"]');
+  let inicio = DateTime.now().toFormat('yyyy-MM-dd');
+  let fin = DateTime.now().plus({months:4}).toFormat('yyyy-MM-dd');
+
+  fechas.forEach(e=>{
+    e.setAttribute("min",inicio);
+    e.setAttribute("max",fin);
+  });
 
 }
 
 //EVENTOS
 window.onload = usuarioLoggeado();
 window.onload = verMisReservas();
+window.onload = configurarFechas();
+
+if (btnCerrarSesion != null) {
+  cerrarSesion.addEventListener("click", () => {
+    sessionStorage.removeItem("mail");
+
+    alert("Has salido de la sesión");
+  });
+}
 
 if (btnBuscar != null) {
   btnBuscar.addEventListener("click", () => {
+    
+    const DateTime = luxon.DateTime;
     reservas.length = 0;
     contenedor.innerHTML = ``;
 
-    let ingreso = new Date(document.getElementById("ingreso").value);
-    let egreso = new Date(document.getElementById("egreso").value);
+    let ingreso = DateTime.fromISO(document.getElementById("ingreso").value);
+    let egreso = DateTime.fromISO(document.getElementById("egreso").value);
     let adultos = document.getElementById("adultos").value;
     let ninos = document.getElementById("ninos").value;
 
     cabanas.forEach((e) => {
-      let fecha1 = new Date(e.ingreso);
-      let fecha2 = new Date(e.egreso);
+      let fecha1 = DateTime.fromISO(e.ingreso);
+      let fecha2 = DateTime.fromISO(e.egreso);
 
-      if (
-        ingreso >= fecha1 &&
-        egreso <= fecha2 &&
-        adultos <= e.adultos &&
-        ninos <= e.ninos
-      ) {
-        const r = new Reservas(
-          e.id,
-          e.src,
-          e.nombre,
-          ingreso,
-          egreso,
-          adultos,
-          ninos
-        );
+      if (ingreso >= fecha1 && egreso <= fecha2 && adultos <= e.adultos && ninos <= e.ninos) {
+        const r = new Reservas(e.id,e.src,e.nombre,ingreso,egreso,adultos,ninos);
 
         reservas.push(r);
-
-        console.log(reservas);
       }
     });
 
     if (reservas.length != 0) {
       reservas.forEach((r) => {
+
+        let {
+          id,
+          src,
+          nombre,
+          ingreso,
+          egreso,
+          adultos,
+          ninos
+        } = r
+
         const card = `<article class="main__reserva-visor-tarjeta">
-            <img class="main__reserva-visor-tarjeta-img" src="${r.src}">
+            <img class="main__reserva-visor-tarjeta-img" src="${src}">
             <table class="main__reserva-visor-tarjeta-tabla">
                 <thead>
                     <th>Nombre</th><th>Entrada</th><th>Salida</th><th>Adultos</th><th>Niños</th><th>Precio</th><th>Acciones</th>
                 </thead>
                 <tbody>
                     <td>${
-                      r.nombre
-                    }</td><td>${r.escribirIngreso()}</td><td>${r.escribirEgreso()}</td><td>${
-          r.adultos
+                      nombre
+                    }</td><td>${(ingreso).toFormat('yyyy-MM-dd')}</td><td>${(egreso).toFormat('yyyy-MM-dd')}</td><td>${
+          adultos
         }</td><td>${
-          r.ninos
+          ninos
         }</td><td>$${r.calcularPrecio()}</td></td><td><button class="main__reserva-visor-tarjeta-tabla-botonR agregar-carrito" id="boton${
-          r.id
+          id
         }">Reservar</button></td>
                 </tbody>
             </table>
@@ -262,11 +246,11 @@ if (btnBuscar != null) {
 
         contenedor.innerHTML += card;
 
-        const btnReservar = document.getElementById(`boton${r.id}`);
+        const btnReservar = document.getElementById(`boton${id}`);
 
         btnReservar.addEventListener("click", () => {
           if (comprobarUsuario()) {
-            guardarReserva(r.id)
+            guardarReserva(id);
           }
         });
       });
@@ -276,26 +260,28 @@ if (btnBuscar != null) {
   });
 }
 
-if(btnIngresar!=null){
-    btnIngresar.addEventListener('click',()=>{
-        
-        let mail = document.getElementById('mail').value;
-        let clave = document.getElementById('clave').value;
+if (btnIngresar != null) {
+  btnIngresar.addEventListener("click", () => {
+    let mail = document.getElementById("mail").value;
+    let clave = document.getElementById("clave").value;
 
-        console.log(mail);
-        console.log(clave);
-
-        if(mail == 'usuario@mail.com' && clave == 'clave123')
-        {
-            sessionStorage.setItem('mail',mail);
-            alert('Ingreso exitoso');
-            location.href = "../index.html";
-            
+    if (mail == "usuario@mail.com" && clave == "clave123") {
+      sessionStorage.setItem("mail", mail);
+      Swal.fire({
+        title:'Ingreso exitoso',
+        icon: 'success',
+        timer: 3000
+      }).then((resultado) => {
+        if(resultado.isConfirmed){
+          location.href = "../index.html";
         }
-        else {
-            alert('Usuario o contraseña inválida. Por favor, intente de nuevo.');
-        }
-
-    });
-
+      })
+    } else {
+      Swal.fire({
+        title: 'Usuario o contraseña inválidos',
+        text: 'Por favor, vuelva a intentar.',
+        icon: 'error'
+      })
+    }
+  });
 }
